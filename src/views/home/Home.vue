4f4e21538e6be89d68b50081c0afb75652d1608a
unclemikael <46005045+unclemikael@users.noreply.github.com>
2020-04-22 00:54:05 +0800

    P153

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,25 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <NavBar class="home-nav">
+      <div slot="center">购物街</div>
+    </NavBar>
+    <h2>首页</h2>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar
+  }
 };
 </script>
-
 <style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
