ea92231d91173f9161cea6ff4bd0de5cff69e7e4
unclemikael <46005045+unclemikael@users.noreply.github.com>
2020-04-22 11:47:27 +0800

    P159

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="tab-control">
+    <div
+      v-for="(title, index) in titles"
+      :key="index"
+      class="tab-control-item"
+      :class="{active: index == currentIndex}"
+      @click="itemClick(index)"
+    >
+      <span>{{title}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    };
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tab-control {
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  background-color: #fff;
+}
+
+.tab-control-item {
+  flex: 1;
+  height: 40px;
+  line-height: 40px;
+}
+
+.active span {
+  color: var(--color-high-text);
+  border-bottom: 3px solid var(--color-tint);
+  padding: 6px;
+}
+</style>
