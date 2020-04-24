34759c598a4c9e5d019c7691b00bc93114e72c42
unclemikael <46005045+unclemikael@users.noreply.github.com>
2020-04-24 12:38:47 +0800

    P179

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>详情页</h2>
+    <div>{{iid}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  data() {
+    return {
+      iid: null
+    };
+  },
+  created() {
+    this.iid = this.$route.params.iid
+  }
+};
+</script>
+
+<style scoped>
+</style>
