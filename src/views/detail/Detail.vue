ba1ffd17dd76effdfe5ea5bbae5e53ad03f74f60
unclemikael <46005045+unclemikael@users.noreply.github.com>
2020-04-24 15:26:39 +0800

    P181

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,24 +1,33 @@
 <template>
   <div id="detail">
     <DetailNavBar></DetailNavBar>
+    <DetailSwiper :topImages="topImages"></DetailSwiper>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComps/DetailNavBar'
-
+import DetailSwiper from './childComps/DetailSwiper'
+import { getDetail } from 'network/detail'
 export default {
   name: "Detail",
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: []
     };
   },
   created() {
     this.iid = this.$route.params.iid;
+    getDetail(this.iid).then(res => {
+      // 1.顶部轮播数据
+      this.topImages = res.result.itemInfo.topImages
+
+    })
   },
   components: {
-    DetailNavBar
+    DetailNavBar,
+    DetailSwiper
   }
 };
 </script>
---
+++ src/views/detail/childComps/DetailSwiper.vue
@@ -0,0 +1,35 @@
+<template>
+  <Swiper class="detail-swiper">
+    <SwiperItem v-for="(topImage, index) in topImages" :key="index">
+      <img :src="topImage" alt />
+    </SwiperItem>
+  </Swiper>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from "components/common/swiper";
+
+export default {
+  name: "DetailSwiper",
+  props: {
+    topImages: {
+      type: Array,
+      default: []
+    }
+  },
+  data() {
+    return {};
+  },
+  components: {
+    Swiper,
+    SwiperItem
+  }
+};
+</script>
+
+<style scoped>
+.detail-swiper {
+  height: 300px;
+  overflow: hidden;
+}
+</style>
