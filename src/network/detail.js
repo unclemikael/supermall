2a2121c4c6ab7f81032f46bd581a384b0fa37cd8
unclemikael <46005045+unclemikael@users.noreply.github.com>
2020-04-24 16:31:40 +0800

    P183

--- src/network/detail.js
+++ src/network/detail.js
@@ -8,3 +8,38 @@ export function getDetail(iid) {
     }
   })
 }
+
+export class Goods {
+  constructor(itemInfo, columns, services) {
+    this.title = itemInfo.title;
+    this.desc = itemInfo.desc;
+    this.newprice = itemInfo.newprice;
+    this.oldPrice = itemInfo.oldPrice;
+    this.discount = itemInfo.discountDesc
+
+    this.columns = columns;
+    this.services = services;
+
+    this.realPrice = itemInfo.lowNowPrice;
+
+  }
+}
+
+export class Shop {
+  constructor(shopInfo) {
+    this.logo = shopInfo.shopLogo; //字符串
+    this.score = shopInfo.score; //数组
+    this.fans = shopInfo.cFans;
+    this.goods = shopInfo.cGoods;
+    this.sells = shopInfo.cSells;
+    this.name = shopInfo.name;
+  }
+}
+
+export class GoodsParam {
+  constructor(info, rule) {
+    this.image = info.images ? info.images[0] : "";
+    this.infos = info.set;
+    this.sizes = rule.tables;
+  }
+}
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,32 +2,49 @@
   <div id="detail">
     <DetailNavBar></DetailNavBar>
     <DetailSwiper :topImages="topImages"></DetailSwiper>
+    <DetailBaseInfo :goods="goods"></DetailBaseInfo>
+    <DetailShopInfo :shop="shop"></DetailShopInfo>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComps/DetailNavBar'
 import DetailSwiper from './childComps/DetailSwiper'
-import { getDetail } from 'network/detail'
+import DetailBaseInfo from './childComps/DetailBaseInfo'
+import DetailShopInfo from './childComps/DetailShopInfo'
+
+import { getDetail, Goods, Shop } from 'network/detail'
 export default {
   name: "Detail",
   data() {
     return {
       iid: null,
-      topImages: []
+      topImages: [],
+      goods: {},
+      shop: {},
     };
   },
   created() {
     this.iid = this.$route.params.iid;
     getDetail(this.iid).then(res => {
-      // 1.顶部轮播数据
-      this.topImages = res.result.itemInfo.topImages
+      console.log(res.result);
 
+      const data = res.result
+      // 1.顶部轮播数据
+      this.topImages = data.itemInfo.topImages
+
+      // 2.获取商品信息
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
+      // 3.创建店铺信息
+      this.shop = new Shop(data.shopInfo)
     })
   },
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo
   }
 };
 </script>
