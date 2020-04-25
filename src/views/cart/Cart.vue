d1c12630888fa24559713fea825deaa5c012eea8
unclemikael <46005045+unclemikael@users.noreply.github.com>
2020-04-26 03:00:47 +0800

    P213

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,12 +1,34 @@
 <template>
-  <h2>购物车</h2>
+  <div id="Cart" class="cart">
+    <NavBar class="nav-bar">
+      <div slot="center">购物车({{cartLength}})</div>
+    </NavBar>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+
+import { mapGetters } from 'vuex'
+
 export default {
-  name: "Cart"
+  name: "Cart",
+  components: {
+    NavBar
+  },
+  computed: {
+    ...mapGetters({
+      cartLength: 'cartLength',
+      cartList: 'cartList'
+    })
+  }
 };
 </script>
 
 <style scoped>
+.nav-bar {
+  background: var(--color-tint);
+  color: white;
+  font-weight: 700;
+}
 </style>
